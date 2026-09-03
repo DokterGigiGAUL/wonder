6f659cd6e50e60d85c65b5c64ed5592465bad1a6
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-09-03 10:18:40 +0700

    Create storage.js

---
+++ js/storage.js
@@ -0,0 +1,90 @@
+const quizzes = [
+    {
+        id: 1,
+        file: "kuis1",
+        productId: "kuis1",
+        title: "Kuis #1",
+        description: "Level: ★★☆☆☆",
+        thumbnail: "assets/images/kuis/thumbnail-kuis1.jpeg",
+        premium: false,
+        price: null,
+        releaseDate: "2026-06-20",
+        type: "quiz"
+    },
+    {
+        id: 2,
+        file: "kuis2",
+        productId: "kuis2",
+        title: "Kuis #2",
+        description: "Level: ★★★☆☆",
+        thumbnail: "assets/images/kuis/thumbnail-kuis2.jpeg",
+        premium: false,
+        price: null,
+        releaseDate: "2026-06-20",
+        type: "quiz"
+    },
+    {
+        id: 3,
+        file: "kuis3",
+        productId: "kuis3",
+        title: "Kuis #3",
+        description: "Level: ★★☆☆☆",
+        thumbnail: "assets/images/kuis/thumbnail-kuis3.jpeg",
+        premium: false,
+        price: null,
+        releaseDate: "2026-06-20",
+        type: "quiz"
+    },
+    {
+        id: 4,
+        file: "kuis4",
+        productId: "kuis4",
+        title: "Kuis #4",
+        description: "Level: ★★★★☆",
+        thumbnail: "assets/images/kuis/thumbnail-kuis4.jpeg",
+        premium: true,
+        price: 5000,
+        mayarUrl: "https://gigital.myr.id/pl/GANTI-DENGAN-LINK-KUIS4", // TODO: ganti dengan payment link Mayar untuk Kuis #4
+        releaseDate: "2026-06-20",
+        type: "quiz"
+    },
+    {
+        id: 5,
+        file: "kuis5",
+        productId: "kuis5",
+        title: "Kuis #5",
+        description: "Level: ★★★☆☆",
+        thumbnail: "assets/images/kuis/thumbnail-kuis5.jpeg",
+        premium: true,
+        price: 5000,
+        mayarUrl: "https://gigital.myr.id/pl/GANTI-DENGAN-LINK-KUIS5", // TODO: ganti dengan payment link Mayar untuk Kuis #5
+        releaseDate: "2026-06-20",
+        type: "quiz"
+    },
+    {
+        id: 6,
+        file: "kuis6",
+        productId: "kuis6",
+        title: "Kuis #6",
+        description: "Level: ★★★★☆",
+        thumbnail: "assets/images/kuis/thumbnail-kuis6.jpeg",
+        premium: true,
+        price: 5000,
+        mayarUrl: "https://gigital.myr.id/pl/GANTI-DENGAN-LINK-KUIS6", // TODO: ganti dengan payment link Mayar untuk Kuis #6
+        releaseDate: "2026-06-20",
+        type: "quiz"
+    },
+    {
+        id: 7,
+        file: "kuis7",
+        productId: "kuis7",
+        title: "Kuis #7",
+        description: "Level: ★★★★★",
+        thumbnail: "assets/images/kuis/thumbnail-kuis7.jpeg",
+        premium: true,
+        price: 5000,
+        mayarUrl: "https://gigital.myr.id/pl/GANTI-DENGAN-LINK-KUIS7", // TODO: ganti dengan payment link Mayar untuk Kuis #7
+        releaseDate: "2026-07-20",
+        type: "quiz"
+    }
+];
