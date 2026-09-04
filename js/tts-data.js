ded001ec1308e3826085c912740d819f3bf07e27
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-09-04 09:08:03 +0700

    Create tts-data.js

---
+++ js/tts-data.js
@@ -0,0 +1,76 @@
+const ttsList = [
+
+    {
+        id: 1,
+        title: "TTS #1",
+        file: "tts1",
+        productId: "tts1",
+        soal: 12,
+        description: "Level: ★☆☆☆☆",
+        thumbnail: "assets/images/tts/D64390B9-FFB2-4D60-8BA0-9F714360822E.png",
+        premium: false,
+        price: null, 
+        hintMayarUrl: "https://gigital.myr.id/pl/tts1", // TODO: link Mayar untuk beli hint/reveal TTS #1
+        releaseDate: "2026-06-20",
+        type: "tts"
+    },
+
+    {
+        id: 2,
+        title: "TTS #2",
+        file: "tts2",
+        productId: "tts2",
+        soal: 11,
+        description: "Level: ★★☆☆☆",
+        thumbnail: "assets/images/tts/D64390B9-FFB2-4D60-8BA0-9F714360822E.png",
+        premium: false,
+        price: null, 
+        hintMayarUrl: "https://gigital.myr.id/pl/tts2", // TODO: link Mayar untuk beli hint/reveal TTS #2
+        releaseDate: "2026-06-20",
+        type: "tts"
+    },
+
+    {
+        id: 3,
+        title: "TTS #3",
+        file: "tts3",
+        productId: "tts3",
+        soal: 12,
+        description: "Level: ★★☆☆☆",
+        thumbnail: "assets/images/tts/D64390B9-FFB2-4D60-8BA0-9F714360822E.png",
+        premium: false,
+        price: null, 
+        hintMayarUrl: "https://gigital.myr.id/pl/tts3", // TODO: link Mayar untuk beli hint/reveal TTS #3
+        releaseDate: "2026-06-20",
+        type: "tts"
+    },
+    {
+        id: 4,
+        title: "TTS #4",
+        file: "tts4",
+        productId: "tts4",
+        soal: 11,
+        description: "Level: ★★★☆☆",
+        thumbnail: "assets/images/tts/D64390B9-FFB2-4D60-8BA0-9F714360822E.png",
+        premium: true,
+        price: 5000, 
+        mayarUrl: "https://gigital.myr.id/pl/tts4", // TODO: link Mayar untuk beli puzzle TTS #4 (hint/reveal otomatis ikut)
+        releaseDate: "2026-08-21",
+        type: "tts"
+    },
+    {
+        id: 5,
+        title: "TTS #5",
+        file: "tts5",
+        productId: "tts5",
+        soal: 12,
+        description: "Level: ★★★★☆",
+        thumbnail: "assets/images/tts/D64390B9-FFB2-4D60-8BA0-9F714360822E.png",
+        premium: true,
+        price: 5000, 
+        mayarUrl: "https://gigital.myr.id/pl/tts5", // TODO: link Mayar untuk beli puzzle TTS #5 (hint/reveal otomatis ikut)
+        releaseDate: "2026-07-22",
+        type: "tts"
+    }
+
+];
