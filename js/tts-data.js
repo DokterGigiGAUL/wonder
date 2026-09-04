dfaa9c6e1ba33df58e4004793ef65fe4d423645e
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-09-04 11:05:53 +0700

    Update titles and release dates in ttsList

--- js/tts-data.js
+++ js/tts-data.js
@@ -2,7 +2,7 @@ const ttsList = [
 
     {
         id: 1,
-        title: "TTS #1",
+        title: "TTS 1",
         file: "tts1",
         productId: "tts1",
         soal: 12,
@@ -17,7 +17,7 @@ const ttsList = [
 
     {
         id: 2,
-        title: "TTS #2",
+        title: "TTS 2",
         file: "tts2",
         productId: "tts2",
         soal: 11,
@@ -32,7 +32,7 @@ const ttsList = [
 
     {
         id: 3,
-        title: "TTS #3",
+        title: "TTS 3",
         file: "tts3",
         productId: "tts3",
         soal: 12,
@@ -46,7 +46,7 @@ const ttsList = [
     },
     {
         id: 4,
-        title: "TTS #4",
+        title: "TTS 4",
         file: "tts4",
         productId: "tts4",
         soal: 11,
@@ -55,12 +55,12 @@ const ttsList = [
         premium: true,
         price: 5000, 
         mayarUrl: "https://gigital.myr.id/pl/tts4", // TODO: link Mayar untuk beli puzzle TTS #4 (hint/reveal otomatis ikut)
-        releaseDate: "2026-08-21",
+        releaseDate: "2026-07-21",
         type: "tts"
     },
     {
         id: 5,
-        title: "TTS #5",
+        title: "TTS 5",
         file: "tts5",
         productId: "tts5",
         soal: 12,
@@ -69,7 +69,7 @@ const ttsList = [
         premium: true,
         price: 5000, 
         mayarUrl: "https://gigital.myr.id/pl/tts5", // TODO: link Mayar untuk beli puzzle TTS #5 (hint/reveal otomatis ikut)
-        releaseDate: "2026-07-22",
+        releaseDate: "2026-08-22",
         type: "tts"
     }
 
