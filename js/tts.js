23dbbf889b62c55a44c1286deba83755d325dac5
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-09-10 00:07:55 +0700

    Update tts.js

--- js/tts.js
+++ js/tts.js
@@ -333,13 +333,6 @@ class CrosswordEngine {
         }
       };
     }
-
-    const backBtn = document.getElementById("backBtn");
-    if (backBtn) {
-      backBtn.onclick = () => {
-        window.location.href = "index.html";
-      };
-    }
     
     const hintLockOverlay = document.getElementById("hintLockModal");
     const hintLockCancel = document.getElementById("hintLockCancel");
