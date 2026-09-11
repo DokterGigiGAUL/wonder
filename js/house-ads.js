2c1d6595f10e55320ae3b8a56c0ba468bb2b9138
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-09-11 12:58:05 +0700

    Update house-ads.js

--- js/house-ads.js
+++ js/house-ads.js
@@ -19,23 +19,23 @@ const houseAds = [
   {
     label: "Iklan",
     title: "Poster Edukasi Gigital",
-    description: "Poster premium Kedokteran Gigi",
-    thumbnail: "#", // TODO: isi link gambar Poster
+    description: "Poster premium Kedokteran Gigi. Ukuran A3.",
+    thumbnail: "https://static.desty.app/desty-omni/20230630/1c4fb8d720c94cdaafe36e3052c78b31.jpg", 
     url: "#" // TODO: isi link pembelian Poster
   },
   {
     label: "Ads",
-    title: "Dentabrix — Dental Chair Berbasis Lego",
-    description: "Koleksi unik untuk pecinta dunia gigi! Model dental chair dari balok Lego, cocok jadi pajangan meja praktik atau kado spesial sesama drg.",
-    thumbnail: "https://down-id.img.susercontent.com/file/id-11134207-8224r-miy0chfmx1j898.webp",
-    url: "https://shopee.co.id/-LIMITED-STOCKS-Dentabrix-Mini-—-Miniatur-Kursi-Dokter-Gigi-i.1515012100.40677691196"
+    title: "Dentabook - Ebook Kedokteran Gigi Gigital",
+    description: "Panduan Peresepan Obat untuk Dokter Gigi",
+    thumbnail: "https://media-myr.b-cdn.net/images/resized/600/6a709c40-0029-4cdc-b274-fe862da5f260.jpeg",
+    url: "https://gigital.myr.id/ebook/ebook-gigital-panduan-peresepan-dokter-gigi"
   },
   {
     label: "Ads",
-    title: "Poster Edukasi Kedokteran Gigi",
-    description: "Poster edukasi bertema kedokteran gigi, cocok untuk klinik, ruang praktik, maupun bahan ajar.",
-    thumbnail: "#", // TODO: isi link gambar Poster
-    url: "#" // TODO: isi link pembelian Poster
+    title: "Dentabook - Ebook Kedokteran Gigi Gigital",
+    description: "Panduan Peresepan Obat untuk Dokter Gigi",
+    thumbnail: "https://media-myr.b-cdn.net/images/resized/600/3a6baa2d-38c9-43a0-90c3-167cad21b2b1.jpeg",
+    url: "https://gigital.myr.id/ebook/ebook-gigital-panduan-manajemen-dental-kehamilan"
   }
   
 ];
