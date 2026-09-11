16f9757f0f4a1737d36f188a5e1a0917a8c230dd
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-09-11 23:14:35 +0700

    Update house-ads.js

--- js/house-ads.js
+++ js/house-ads.js
@@ -11,7 +11,7 @@
 const houseAds = [
   {
     label: "Iklan",
-    title: "Lego Dental Chair",
+    title: "Dentabrix - Lego Dental Chair",
     description: "Dentabrix Gigital Mini",
     thumbnail: "https://down-id.img.susercontent.com/file/id-11134207-8224r-miy0chfmx1j898.webp",
     url: "https://shopee.co.id/-LIMITED-STOCKS-Dentabrix-Mini-—-Miniatur-Kursi-Dokter-Gigi-i.1515012100.40677691196"
@@ -39,14 +39,14 @@ const houseAds = [
   },
   {
     label: "Ads",
-    title: "Ebook Kedokteran Gigi Gigital",
+    title: "Ebook Kedokteran Gigi",
     description: " Panduan Prinsip Tatalaksana Lesi Oral",
     thumbnail: "https://media-myr.b-cdn.net/images/resized/600/98236151-2996-436d-91be-5bf8e506b999.jpeg",
     url: "https://gigital.myr.id/ebook/ebook-gigital-tatalaksana-lesi-oral"
   },
   {
     label: "Iklan",
-    title: "Ebook Kedokteran Gigi Gigital",
+    title: "Ebook Kedokteran Gigi",
     description: "Manajemen Dental pada Kehamilan",
     thumbnail: "https://media-myr.b-cdn.net/images/resized/600/3a6baa2d-38c9-43a0-90c3-167cad21b2b1.jpeg",
     url: "https://gigital.myr.id/ebook/ebook-gigital-panduan-manajemen-dental-kehamilan"
