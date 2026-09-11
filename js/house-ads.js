adf96f37026abdbe82d725371c5763061e649574
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-09-11 11:43:21 +0700

    Update house-ads.js

--- js/house-ads.js
+++ js/house-ads.js
@@ -12,9 +12,16 @@ const houseAds = [
   {
     label: "Iklan",
     title: "Dentabrix — Dental Chair Berbasis Lego",
-    description: "Koleksi unik untuk pecinta dunia gigi! Model dental chair dari balok Lego, cocok jadi pajangan meja praktik atau kado spesial sesama drg.",
+    description: "Koleksi unik bertema Kedokteran Gigi",
     thumbnail: "https://down-id.img.susercontent.com/file/id-11134207-8224r-miy0chfmx1j898.webp",
     url: "https://shopee.co.id/-LIMITED-STOCKS-Dentabrix-Mini-—-Miniatur-Kursi-Dokter-Gigi-i.1515012100.40677691196"
+  },
+  {
+    label: "Iklan",
+    title: "Poster Edukasi Gigital",
+    description: "Poster premium Kedokteran Gigi",
+    thumbnail: "#", // TODO: isi link gambar Poster
+    url: "#" // TODO: isi link pembelian Poster
   },
   {
     label: "Ads",
@@ -23,14 +30,6 @@ const houseAds = [
     thumbnail: "https://down-id.img.susercontent.com/file/id-11134207-8224r-miy0chfmx1j898.webp",
     url: "https://shopee.co.id/-LIMITED-STOCKS-Dentabrix-Mini-—-Miniatur-Kursi-Dokter-Gigi-i.1515012100.40677691196"
   },
-  {
-    label: "Iklan",
-    title: "Poster Edukasi Kedokteran Gigi",
-    description: "Poster edukasi bertema kedokteran gigi, cocok untuk klinik, ruang praktik, maupun bahan ajar.",
-    thumbnail: "#", // TODO: isi link gambar Poster
-    url: "#" // TODO: isi link pembelian Poster
-  },
-  
   {
     label: "Ads",
     title: "Poster Edukasi Kedokteran Gigi",
