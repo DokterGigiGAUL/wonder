45ab4dea1aedaa6b4beed68afb9451c2e7365e9f
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-09-12 19:02:03 +0700

    Update lego-data.js

--- js/lego-data.js
+++ js/lego-data.js
@@ -7,24 +7,24 @@
 const legoSets = [
     {
         id: "Lego-001",
-        title: "Dentabrix Mini",
-        description: "Deskripsi singkat produk.",
+        title: "Mini",
+        description: "Minimal. Maksimal.",
         thumbnail: "assets/images/lego/lego-001.jpg",
         price: 150000,
         buyUrl: "#"
     },
       {
         id: "Lego-002",
-        title: "Dentabrix Modern",
-        description: "Deskripsi singkat produk.",
+        title: "Modern",
+        description: "Kursi Gigi Banget.",
         thumbnail: "assets/images/lego/lego-001.jpg",
         price: 300000,
         buyUrl: "#"
     },
       {
         id: "Lego-003",
-        title: "Dentabrix Retro",
-        description: "Deskripsi singkat produk.",
+        title: "Retro",
+        description: "Yang Tua Tua Aja.",
         thumbnail: "assets/images/lego/lego-001.jpg",
         price: 350000,
         buyUrl: "#"
