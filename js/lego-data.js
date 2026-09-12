b28a45c982736caa4c7482dd6beedd72e8f14085
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-09-12 23:11:41 +0700

    Update lego-data.js

--- js/lego-data.js
+++ js/lego-data.js
@@ -8,26 +8,26 @@ const legoSets = [
     {
         id: "Lego-001",
         title: "Mini",
-        description: "Minimal. Maksimal.",
+        description: "Minimal Maksimal.",
         thumbnail: "assets/images/Mini.webp",
         price: 150000,
-        buyUrl: "#"
+        buyUrl: "https://id.shp.ee/qazhVmbL"
     },
       {
         id: "Lego-002",
         title: "Modern",
         description: "Kursi Gigi Banget.",
-        thumbnail: "assets/images/lego/lego-001.jpg",
+        thumbnail: "assets/images/Modern.webp",
         price: 300000,
-        buyUrl: "#"
+        buyUrl: "https://id.shp.ee/qazhVmbL"
     },
       {
         id: "Lego-003",
         title: "Retro",
         description: "Yang Tua Tua Aja.",
-        thumbnail: "assets/images/lego/lego-001.jpg",
+        thumbnail: "assets/images/Retro.webp",
         price: 350000,
-        buyUrl: "#"
+        buyUrl: "https://id.shp.ee/qazhVmbL"
     }
     // tambah item lain di sini
 ];
